7b2e6154c4be895a5c2a0a7dd3471f39f5ba5989
osebeantony <osebe.anto@gmail.com>
2024-10-25 10:51:30 +0300

    minor fixes

--- src/components/cropCalender.js
+++ src/components/cropCalender.js
@@ -18,7 +18,7 @@ const images = [
   { id: 2, src: image7, text: "Unlock Your Farming Potential" },
   { id: 3, src: image6, text: "Maximize Your Yields Today" },
 ];
-const CropCalendar = () => {
+const CropCalender = () => {
   return (
     <div>
       <Swiper
@@ -200,4 +200,4 @@ const CropCalendar = () => {
   );
 };
 
-export default CropCalendar;
+export default CropCalender;
